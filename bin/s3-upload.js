8de997048d84d4e80905a725b93241f8b0b6654b
andreasantarl <andrea.santarl@gmail.com>
2016-07-08 15:29:04 -0400

    Refactor into an upload module
    This module is usable from both a command-line application and an express
    api server.

--- bin/s3-upload.js
+++ bin/s3-upload.js
@@ -1,40 +1,9 @@
 'use strict';
 
-require('dotenv').config();
+const uploader = require('../lib/aws-s3-upload');
 
 //node modules
 const fs = require('fs');
-const crypto = require('crypto');
-
-//npm modules
-const fileType = require('file-type');
-const AWS = require('aws-sdk');
-
-const s3 = new AWS.S3({
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  },
-}); //constructor function
-
-const mimeType = (data) => {
-  return Object.assign({  //Amazon needs to know extension and mime type to save to bucket, so set defaults
-    ext: 'bin',   //binary default
-    mime: 'application/octet-stream'  //stream of bits default
-  }, fileType(data));   //merge/overwrite first object with second object if don't have something in data: object that is returned with extension and mime type
-  // by default, returns object based on NPM extension file-type
-};
-
-const randomHexString = (length) => {
-  return new Promise ((resolve, reject) => {
-    crypto.randomBytes(length, (error, buffer) => {
-      if (error) {
-        reject(error);
-      }
-      resolve(buffer.toString('hex'));  //buffer does not return a string, so need to coerce it
-    });
-  });
-};
 
 let filename = process.argv[2] || '';
 
@@ -49,39 +18,8 @@ const readFile = (filename) => {
   });
 };
 
-const awsUpload = (file) => {
-
-  return randomHexString(16)
-  .then((filename) => {
-    let dir = new Date().toISOString().split('T')[0];
-    return {
-      ACL: "public-read",
-      Body: file.data,
-      Bucket: 'andreasantarlasci',
-      ContentType: file.mime,
-      Key: `${dir}/${filename}.${file.ext}`,
-    };
-  })
-  .then((options) => {
-    return new Promise ((resolve, reject) => {
-        s3.upload(options, (error, data) => {
-          if (error) {
-            reject(error);
-          }
-          resolve(data);
-        });
-      });
-  });
-};
-
 readFile(filename)
-.then((data) => {
-  //extra wrapper for filetype - files can be corrupted, etc, so need to give default filetype
-  let file = mimeType(data);
-  file.data = data;
-  return file;
-})
-.then(awsUpload)
+.then(uploader.prepareFile)
+.then(uploader.awsUpload)
 .then(console.log)
-//.then((data) => console.log(`${filename} is ${data.length} byes long`))
 .catch(console.error);
