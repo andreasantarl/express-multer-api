7bc249dc4cb5c96c994acf09799d3fa32ba868df
andreasantarl <andrea.santarl@gmail.com>
2016-07-08 13:09:37 -0400

    Determines file/mime type, starts AWS upload fnct
    Use file-type module to determine the content-type of the upload.
    
    Configured options for our upload based on our bucket name.

--- bin/s3-upload.js
+++ bin/s3-upload.js
@@ -1,9 +1,17 @@
 'use strict';
 
 const fs = require('fs');
+const fileType = require('file-type');
 
 let filename = process.argv[2] || '';
 
+const mimeType = (data) => {
+  return Object.assign({  //Amazon needs to know extension and mime type to save to bucket, so set defaults
+    ext: 'bin',   //binary default
+    mime: 'application/octet-stream'  //stream of bits default
+  }, fileType(data));   //merge/overwrite first object with second object if don't have something in data: object that is returned with extension and mime type
+  // by default, returns object based on NPM extension file-type
+};
 
 const readFile = (filename) => {
   return new Promise((resolve, reject) => {
@@ -16,6 +24,26 @@ const readFile = (filename) => {
   });
 };
 
+const awsUpload = (file) => {
+  const options = {
+    ACL: "public-read",
+    Body: file.data,
+    Bucket: 'andreasantarlasci',
+    ContentType: file.mime,
+    Key: `test/test.${file.ext}`,
+  };
+  return Promise.resolve(options);
+
+};
+
 readFile(filename)
-.then((data) => console.log(`${filename} is ${data.length} byes long`))
+.then((data) => {
+  //extra wrapper for filetype - files can be corrupted, etc, so need to give default filetype
+  let file = mimeType(data);
+  file.data = data;
+  return file;
+})
+.then(awsUpload)
+.then(console.log)
+//.then((data) => console.log(`${filename} is ${data.length} byes long`))
 .catch(console.error);
